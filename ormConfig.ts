43bc4fcbb28a350dcbdedc5e9fabb6421ebd7e63
owolabi2001 <owolabitemiolamide@gmail.com>
2024-04-30 18:21:39 +0100

    first commit

---
+++ ormConfig.ts
@@ -0,0 +1,11 @@
+import { DataSource } from "typeorm";
+
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "test",
+    password: "test",
+    database: "test",
+})
